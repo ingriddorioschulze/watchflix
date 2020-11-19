43a8999339d785828d9bbf8387f35a4eec253327
Ingrid do Rio Schulze <ingriddorio@gmail.com>
2020-11-19 17:59:37 +0100

    Added Row component and fetched the api data to display the film images

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React from 'react'
 import styled from 'styled-components'
+import Row from './Row'
+import Banner from './Banner'
+import NavBar from './NavBar'
+import requests from './requests'
 
+const AppWrapper = styled.div`
+  background-color: #050404;
+
+  h1 {
+    color: #ffb100;
+    font-family: 'Bebas Neue', cursive;
+  }
+`
 
 function App() {
   return (
-    <div>
-        <h1>WATCHFLIX</h1>
-    </div>
-  );
+    <AppWrapper>
+      <h1>WATCHFLIX</h1>
+      <NavBar />
+      <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+    </AppWrapper>
+  )
 }
 
-export default App;
+export default App
---
+++ src/Row.js
@@ -0,0 +1,75 @@
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import axios from './axios'
+
+const base_url = 'https://image.tmdb.org/t/p/original/'
+
+//#region styles
+const RowWrapper = styled.div`
+  margin-left: 20px;
+
+  h2 {
+    font-size: 16px;
+    padding-left: 20px;
+    color: #ffb100;
+  }
+`
+const RowPostersWrapper = styled.div`
+  display: flex;
+  overflow-y: hidden;
+  overflow-x: scroll;
+  padding: 20px;
+  ::-webkit-scrollbar {
+    display: none;
+  }
+  .row-poster {
+    width: 100%;
+    max-height: 100px;
+    margin-right: 10px;
+    object-fit: contain;
+    transition: transform 450ms;
+    :hover {
+      transform: scale(1.08);
+    }
+  }
+  .row-poster-large {
+    max-height: 250px;
+    :hover {
+      transform: scale(1.09);
+    }
+  }
+`
+//#endregion
+
+function Row({ title, fetchUrl, isLargeRow }) {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchUrl)
+      setMovies(request.data.results)
+      return request
+    }
+    fetchData()
+  }, [fetchUrl])
+
+  return (
+    <RowWrapper>
+      <h2>{title}</h2>
+      <RowPostersWrapper>
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className={`row-poster ${isLargeRow && 'row-poster-large'}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+          />
+        ))}
+      </RowPostersWrapper>
+    </RowWrapper>
+  )
+}
+
+export default Row
